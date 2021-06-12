4671646527374cc7d2eb42ee9854289dc09cbafd
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-06-12 16:30:51 +0530

    add logic, but still the clock speed is proportional to the clicks

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
-document.getElementsByClassName("start").addEventListener("click" , startTimer);
+let minutes = 25;
+let seconds = "00";
 
-function startTimer(){
-    alert("start the timer");
-}                    
+ function template(){
+     document.getElementById("minutes").innerHTML = minutes;
+     document.getElementById("seconds").innerHTML = seconds;
+ }
+
+ function start(){
+      
+    minutes = 24;
+    seconds = 59;
+
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    let minutes_interval = setInterval(minutesTimer , 60000);
+    let seconds_interval = setInterval(secondsTimer , 1000);
+
+    function minutesTimer(){
+        minutes = minutes - 1;
+
+        document.getElementById("minutes").innerHTML = minutes;
+
+        if(minutes <= 0){
+            minutes = 25;
+        }
+     }
+     function secondsTimer(){
+        seconds = seconds - 1;
+
+        document.getElementById("seconds").innerHTML = seconds;
+
+    
+        if(seconds <= 0){
+            if(minutes == 0){
+                clearInterval(minutes_interval);
+                clearInterval(seconds_interval);
+
+                document.getElementById("done").innerHTML =  "Take a break";
+
+                document.getElementById("done").classList.add("show_message");
+            }
+            seconds = 60;
+        }
+         
+     }
+ }
+
+ function reset(){
+
+    minutes = 24;
+    seconds = "00";
+
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+
+ }
 
