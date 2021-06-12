3ac6bf0548e4a3587a6093a59f11820c6c14cbec
Siddhant <siddhant@Siddhants-MacBook-Air.local>
2021-06-12 10:01:22 +0530

    js

---
+++ app.js
@@ -0,0 +1,11 @@
+// let timer = document.querySelector("start");
+
+// function  startTimer(){
+//     alert("i got clicked");
+// }                    
+
+document.getElementsByClassName("start").addEventListener("click" , startTimer);
+
+function startTimer(){
+    alert("i got clicked");
+}                    
